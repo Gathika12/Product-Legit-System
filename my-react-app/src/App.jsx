c61000075285cd3234e332192cbfbfa2642a6a1c
Gathika12 <gathikacolambage@gmail.com>
2024-08-10 22:55:43 -0700

    Developing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+// QRScanner.js
+
+import React, { useState } from 'react';
+import { Html5QrcodeScanner } from 'html5-qrcode';
 
 const App = () => {
-  return (
-    <div>
-      <form action="">
-        Name<input type="text" name="" id="" />
-        Code<input type="text" name="" id="" />
-      </form>
-    </div>
-  )
-}
+    const [message, setMessage] = useState('');
 
-export default App
+    const onScanSuccess = (decodedText) => {
+        fetch(`http://localhost/qr/handleScan.php?product_id=${decodedText}`)
+            .then(response => response.text())
+            .then(data => {
+                setMessage(data);
+            })
+            .catch(error => console.error('Error:', error));
+    };
+
+    React.useEffect(() => {
+        const scanner = new Html5QrcodeScanner(
+            "reader", { fps: 10, qrbox: 250 });
+        scanner.render(onScanSuccess);
+
+        return () => scanner.clear();
+    }, []);
+
+    return (
+        <div>
+            <div id="reader"></div>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default App;
